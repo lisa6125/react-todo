89f5246d5c97351342d40c60109db226ffff7c0a
alice wang <alice@staging.com.tw>
2022-08-24 18:23:58 +0800

    todo 初始刻板完成

--- src/pages/Todo/StyledTodo.js
+++ src/pages/Todo/StyledTodo.js
@@ -1,3 +1,139 @@
 import styled from 'styled-components';
 
-export const StyledTodo = styled.div``
+export const StyledTodo = styled.div`
+*{
+box-sizing: border-box;
+}
+.todo{
+    background: #FFD370;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+    z-index: 1;
+    &::before{
+        content:'';
+        z-index: -1;
+        position: absolute;
+        bottom:0;
+        left: 0;
+        width: 100%;
+        height: 70%;
+        background-color: #fff;
+        clip-path: polygon(0 60%, 100% 15%, 100% 100%, 0% 100%);
+    }
+    &_container{
+        margin: 0 auto;
+        max-width: 500px;
+        width: 80%;
+        z-index: 5;
+    }
+    &_input{
+        position: relative;
+        input{
+            width: 100%;
+            height: 47px;
+            background: #FFFFFF;
+            box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+            border-radius: 10px;
+            border: none;
+            outline: none;
+            padding:12px 16px;
+            color: #9F9A91;
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 23px;
+        }
+        .addIcon{
+            position: absolute;
+            top: 4px;
+            right: 4px;
+            width: 40px;
+            height: 40px;
+            cursor: pointer;
+        }
+    }
+    &_listBox{
+        height: 100%;
+        max-height: 445px;
+        width: 100%;
+        background: #FFFFFF;
+        box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+        border-radius: 10px;
+        margin-top: 16px;
+        position: relative;
+        padding-bottom: 71px;
+        &_tab{
+            width: 100%;
+            border-bottom: 2px solid #EFEFEF;
+            &_item{
+                width: 33.33%;
+                display: inline-block;
+                font-weight: 700;
+                font-size: 14px;
+                line-height: 20px;
+                text-align: center;
+                color: #9F9A91;
+                padding: 16px 0;
+                cursor: pointer;
+            }
+        }
+        &_list{
+            .empty{
+                text-align: center;
+                padding: 24px 0;
+            }
+        }
+        &_done{
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            margin: 0 24px;
+            padding: 25px 0;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            width:calc(100% - 48px);
+            border-top: 1px solid #EFEFEF;
+            .haveDoneNum{
+                font-weight: 400;
+                font-size: 14px;
+                line-height: 20px;
+                color: #333333;
+            }
+            .clearDone{
+                font-weight: 400;
+                font-size: 14px;
+                line-height: 20px;
+                color: #9F9A91;
+            }
+        }
+    }
+    .nav{
+        width: 100%;
+        height: 72px;
+        padding:16px 34px;
+        display: flex;
+        &_logo{
+            flex-grow: 1;
+            cursor: pointer;
+        }
+        &_user{
+            margin-right: 24px;
+            font-weight: 700;
+            font-size: 16px;
+            line-height: 23px;
+            color: #333333;
+            cursor: pointer;
+        }
+        &_logout{
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 23px;
+        color: #333333;
+        cursor: pointer;
+        }
+    }
+}
+
+`
--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { StyledTodo } from './StyledTodo'
 
 export default function Todo() {
+
+    const [todoArr, setTodoArr] = useState([]);
     return (
-    <StyledTodo>
-        <div className='Todo'>todo</div>
-    </StyledTodo>
+        <StyledTodo>
+            <div className='todo'>
+                <div className="nav">
+                    <div className="nav_logo">
+                        <img src="./assets/images/logo.svg" alt="" />
+                    </div>
+                    <div className="nav_user">
+                        王小明的代辦
+                    </div>
+                    <div className="nav_logout">登出</div>
+                </div>
+                <div className="todo_container">
+                    <div className="todo_input">
+                        <input type="text" placeholder='新增待辦事項' />
+                        <div className="addIcon">
+                            <img src="./assets/images/add.svg" alt="" />
+                        </div>
+                    </div>
+                    <div className="todo_listBox">
+                        <div className="todo_listBox_tab">
+                            <div className="todo_listBox_tab_item">全部</div>
+                            <div className="todo_listBox_tab_item">待完成</div>
+                            <div className="todo_listBox_tab_item">已完成</div>
+                        </div>
+                        <div className="todo_listBox_list">
+                            {
+                                todoArr.length > 0 ?
+                                    todoArr.map(() => { return (<></>) }
+                                    ) :
+                                    <div className="empty">
+                                        <img src="./assets/images/empty.png" alt="" />
+                                    </div>
+                            }
+                        </div>
+                        <div className="todo_listBox_done">
+                            <div className="haveDoneNum">5 個待完成項目</div>
+                            <div className="clearDone">清除已完成項目</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </StyledTodo>
     )
 }
