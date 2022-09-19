b8046da3aaa7e943dca6ea4b80db974a9df4d1eb
lisa6125 <fxck6125@gmail.com>
2022-09-19 10:56:34 +0800

    新增 todo api

---
+++ src/api/addTodo.js
@@ -0,0 +1,29 @@
+//plugin
+import Cookies from 'js-cookie';
+import axios from 'axios';
+// 提示
+import toast from 'react-hot-toast';
+
+const addTodo = async (content) => {
+  let token = Cookies.get('token');
+  let res;
+  let body = {
+    todo: {
+      content: content,
+    },
+  };
+  body = JSON.stringify(body);
+  await axios
+    .post('https://todoo.5xcamp.us/todos', body, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    })
+    .then((response) => {
+      res = response.data;
+    });
+  return res;
+};
+
+export default addTodo;
---
+++ src/api/getTodoList.js
@@ -0,0 +1,21 @@
+//plugin
+import Cookies from 'js-cookie';
+import axios from 'axios';
+
+const getTodoList = async () => {
+  let token = Cookies.get('token');
+  let res;
+  await axios
+    .get('https://todoo.5xcamp.us/todos', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    })
+    .then((response) => {
+      res = response.data.todos;
+    });
+  return res;
+};
+
+export default getTodoList;
